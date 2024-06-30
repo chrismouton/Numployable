6c306df5fb671b96d35bdd1782e89a4e8625ff95
Chris Mouton <chrismouton74@gmail.com>
2024-06-30 19:58:50 +0100

    Adding logic to start up API

---
+++ src/API/API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,60 @@
+namespace Numployable.API.Middleware;
+
+using System.Net;
+
+using Newtonsoft.Json;
+
+using Numployable.Application.Exceptions;
+
+public class ExceptionMiddleware(RequestDelegate next)
+{
+    private readonly RequestDelegate _next = next;
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(httpContext, ex);
+        }
+    }
+    
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+        string result = JsonConvert.SerializeObject(new ErrorDetails
+        {
+            ErrorMessage = exception.Message,
+            ErrorType = "Failure"
+        });
+
+        switch (exception)
+        {
+            case BadRequestException badRequestException:
+                statusCode = HttpStatusCode.BadRequest;
+                break;
+            case ValidationException validationException:
+                statusCode = HttpStatusCode.BadRequest;
+                result = JsonConvert.SerializeObject(validationException.Errors);
+                break;
+            case NotFoundException notFoundException:
+                statusCode = HttpStatusCode.NotFound;
+                break;
+            default:
+                break;
+        }
+
+        context.Response.StatusCode = (int)statusCode;
+        return context.Response.WriteAsync(result);
+    }
+}
+
+public class ErrorDetails
+{
+    public required string ErrorType { get; set; }
+    public required string ErrorMessage { get; set; }
+}
---
+++ src/API/API/Startup.cs
@@ -0,0 +1,107 @@
+namespace Numployable.API;
+
+using Microsoft.OpenApi.Models;
+
+using Numployable.API.Middleware;
+using Numployable.Application;
+using Numployable.Persistence;
+
+public class Startup(IConfiguration configuration)
+{
+    public readonly IConfiguration _configuration = configuration;
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddHttpContextAccessor();
+
+        AddSwaggerDoc(services);
+
+        services.ConfigureApplicationServices();
+        //services.ConfigureInfrastructureServices(_configuration); - No infrastrucutre yet
+        services.ConfigurePersistenceServices(_configuration);
+        //services.ConfigureIdentityServices(_configuration); - Coming later
+
+        services.AddControllers();
+
+        services.AddCors(o =>
+        {
+            o.AddPolicy("CorsPolicy",
+                builder => builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+        });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseMiddleware<ExceptionMiddleware>();
+
+        app.UseAuthentication();
+
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Numployable.Api v1"));
+
+        //app.UseHttpsRedirection(); - Currently broken
+
+        app.UseRouting();
+
+        //app.UseAuthorization();
+
+        app.UseCors("CorsPolicy");
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+
+    private static void AddSwaggerDoc(IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = @"JWT Authorization header using the Bearer scheme. 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 12345abcdef'",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+              {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                          {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                          },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                      }
+                });
+
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "Numployable Api",
+
+            });
+
+        });
+    }
+}
